b307d2f14f1232678fa5ab7405ab35d61f5571aa
samad817 <samad2006abdl@gmail.com>
2026-06-22 14:19:59 +0530

    feat: implement simple wire continuous assignment
    Connected the input pin directly to the output pin using a basic continuous assign statement.

---
+++ basics/simple_wire.v
@@ -0,0 +1,7 @@
+module top_module( input in, output out );
+assign out=in;
+endmodule
+/* 
+   Implements a continuous assignment to create a simple wire 
+   connecting the input signal directly to the output.
+*/
