b1962d164146c70976a968bda6b190d22c4b31e9
samad817 <samad2006abdl@gmail.com>
2026-07-02 22:12:13 +0530

    feat: solve always block1 demonstrating combinational design patterns

---
+++ procedures/always_block1.v
@@ -0,0 +1,15 @@
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    assign out_assign = a & b;// using assign statement
+    
+    always @(*) begin // using combinational block 
+        out_alwaysblock = a & b;
+    end
+    
+
+endmodule
