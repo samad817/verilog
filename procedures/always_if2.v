4bf7359c03984644f48c920e7ce4d48a9ef4428b
samad817 <samad2006abdl@gmail.com>
2026-07-02 22:27:39 +0530

    feat: solve always if2 by eliminating inferred latches with default values

---
+++ procedures/always_if2.v
@@ -0,0 +1,21 @@
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); //
+
+    always @(*) begin
+        shut_off_computer = 1'b0;
+        if (cpu_overheated)
+           shut_off_computer = 1;
+    end
+
+    always @(*) begin
+      keep_driving = 1'b0;
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+    end
+
+endmodule
